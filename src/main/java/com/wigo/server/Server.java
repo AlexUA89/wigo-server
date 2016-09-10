b0bf1553f254e0659d685df35944b233a225d587
alyaxey <aliaxey90@gmail.com>
2016-09-10 15:17:43 +0300

    added dummy server

--- src/main/java/com/wigo/server/Server.java
+++ src/main/java/com/wigo/server/Server.java
@@ -1,12 +1,44 @@
 package com.wigo.server;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import spark.Request;
+import spark.Response;
+
+import java.time.Instant;
+import java.util.Date;
 
 import static spark.Spark.*;
 
 public class Server {
     public static void main(String[] args) {
-        Gson gson = new Gson();
-        get("/api/status", (req, res) -> {return ""+req.queryParams("latitude_from");}, gson::toJson);
+        System.out.println(System.currentTimeMillis());
+        Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ssX").create();
+        get("/api/status", Server::getStatus, gson::toJson);
+        get("/api/status/:id/message", Server::getStatusMessage, gson::toJson);
+    }
+
+    private static Object getStatus(Request req, Response res) {
+        return ImmutableMap.of("list",
+                ImmutableList.of(
+                        ImmutableMap.builder().put("id", "er70hserh")
+                                .put("latitude", 12.345)
+                                .put("longitude", 67.890)
+                                .put("name", "hello, world!")
+                                .put("text", "good place!!!")
+                                .put("start_date", "2012-04-23T18:25:43.511Z")
+                                .put("end_date", "2012-04-23T18:25:43.511Z")
+                                .put("user", ImmutableMap.of("id", "r0her78er6", "name", "John")).build()));
+    }
+
+    private static Object getStatusMessage(Request req, Response res) {
+        return ImmutableMap.of("list", ImmutableList.of(ImmutableMap.of(
+                "text", "hi!!",
+                "user", ImmutableMap.of("id", "r0her78er6", "name", "John"),
+                "date", Date.from(Instant.parse("2012-04-23T18:25:43.511Z"))
+        )));
     }
 }
