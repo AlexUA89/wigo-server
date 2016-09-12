915286fe57e9149639f6765b7900f525f30a35ba
AlexUA <alexua89@gmail.com>
2016-09-12 18:24:13 +0300

    setup server

--- scripts/pullAndRestart.sh
+++ scripts/pullAndRestart.sh
@@ -4,6 +4,7 @@ git pull https://2b6697f81aa09ea18fc28e8d5666fb8e7650e220@github.com/AlexUA89/wi
 mvn clean package
 rm -r /root/apache-tomcat-9.0.0.M10/temp
 rm -r /root/apache-tomcat-9.0.0.M10/work
-cp -a /root/wigo-server/target/server-1.0-SNAPSHOT.war /root/apache-tomcat-9.0.0.M10/webapps
-chmod a+x /root/apache-tomcat-9.0.0.M10/webapps/server-1.0-SNAPSHOT.war
+rm -r /root/apache-tomcat-9.0.0.M10/wigo-server
+rm -r /root/apache-tomcat-9.0.0.M10/wigo-server.war
+cp -a /root/wigo-server/target/wigo-server.war /root/apache-tomcat-9.0.0.M10/webapps
 sh /root/apache-tomcat-9.0.0.M10/bin/startup.sh
