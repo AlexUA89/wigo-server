59cf0f48c7366ca00cae37be7b718ffd11002d43
AlexUA <alexua89@gmail.com>
2016-09-12 14:43:42 +0300

    setup server

--- scripts/pullAndRestart.sh
+++ scripts/pullAndRestart.sh
@@ -1,5 +1,7 @@
-
-
+sh /root/apache-tomcat-9.0.0.M10/bin/startup.sh
+cd /root/wigo-server
 git pull https://2b6697f81aa09ea18fc28e8d5666fb8e7650e220@github.com/AlexUA89/wigo-server.git
-
-mvn clean install spring-boot:run
+mvn clean package
+rm -r /root/apache-tomcat-9.0.0.M10/temp
+rm -r /root/apache-tomcat-9.0.0.M10/work
+cp -a /source/. /dest/
--- src/main/java/com/wigo/server/WigoApp.java
+++ src/main/java/com/wigo/server/WigoApp.java
@@ -4,18 +4,29 @@ import com.wigo.server.config.WigoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @Import({WigoConfiguration.class})
-public class Boot {
+public class WigoApp extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Boot.class, args);
+        ApplicationContext ctx = SpringApplication.run(WigoApp.class, args);
         System.out.println("Wigo server started on localhost:8080");
         System.out.println("Visit /swagger-ui.html fro more details");
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(appClass);
+    }
+
+    private static Class<WigoApp> appClass = WigoApp.class;
 
 }
