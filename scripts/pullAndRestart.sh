e644f3a1c3928441aaa45ef6954a6177b6898bf0
alyaxey <aliaxey90@gmail.com>
2016-09-15 07:18:29 +0300

    fixed pull script

--- scripts/pullAndRestart.sh
+++ scripts/pullAndRestart.sh
@@ -2,9 +2,9 @@ sh /root/apache-tomcat-9.0.0.M10/bin/shutdown.sh
 cd /root/wigo-server
 git pull https://2b6697f81aa09ea18fc28e8d5666fb8e7650e220@github.com/AlexUA89/wigo-server.git
 mvn clean package
-rm -r /root/apache-tomcat-9.0.0.M10/temp
-rm -r /root/apache-tomcat-9.0.0.M10/work
-rm -r /root/apache-tomcat-9.0.0.M10/wigo-server
-rm -r /root/apache-tomcat-9.0.0.M10/wigo-server.war
+rm -rf /root/apache-tomcat-9.0.0.M10/temp
+rm -rf /root/apache-tomcat-9.0.0.M10/work
+rm -rf /root/apache-tomcat-9.0.0.M10/webapps/wigo-server
+rm -rf /root/apache-tomcat-9.0.0.M10/webapps/wigo-server.war
 cp -a /root/wigo-server/target/wigo-server.war /root/apache-tomcat-9.0.0.M10/webapps
 sh /root/apache-tomcat-9.0.0.M10/bin/startup.sh
