46262e9bc0d7b0298c9b22c8b8a321e7d9c3d93f
alyaxey <aliaxey90@gmail.com>
2017-01-22 18:57:19 +0200

    updated restart script

--- scripts/pullAndRestart.sh
+++ scripts/pullAndRestart.sh
@@ -1,3 +1,5 @@
+set -eo pipefail
+
 TOMCAT_DIR="/home/ubuntu/apache-tomcat-9.0.0.M13"
 WIGO_DIR="/home/ubuntu/wigo-server"
 
@@ -7,7 +9,12 @@ git pull https://2b6697f81aa09ea18fc28e8d5666fb8e7650e220@github.com/AlexUA89/wi
 mvn clean package
 rm -rf "$TOMCAT_DIR/temp"
 rm -rf "$TOMCAT_DIR/work"
-rm -rf "$TOMCAT_DIR/webapps/wigo-server"
-rm -rf "$TOMCAT_DIR/webapps/wigo-server.war"
-cp -a "$WIGO_DIR/target/wigo-server.war" "$TOMCAT_DIR/webapps"
+rm -rf "$TOMCAT_DIR/webapps/ROOT"
+mkdir "$TOMCAT_DIR/webapps/ROOT"
+cp -a "$WIGO_DIR/target/wigo-server.war" "$TOMCAT_DIR/webapps/ROOT"
+cd "$TOMCAT_DIR/webapps/ROOT"
+unzip wigo-server.war
+rm wigo-server.war
+cp -r /home/ubuntu/wigo-static/* .
+sed -i 's|http://52.90.115.129:8080/wigo-server||g' static/js/app.b2d4054c2e74e02cf8fc.js*
 sh "$TOMCAT_DIR/bin/startup.sh"
