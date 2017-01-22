9fc6285a5dfcbdf6856daf95b63ac9eb545ad198
alyaxey <aliaxey90@gmail.com>
2017-01-22 19:30:36 +0200

    saved instructions for the future

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -3,3 +3,18 @@ psql postgres -f create_tables.sql
 
 create tables for ubuntu:
 sudo -i -u postgres psql -f create_tables.sql
+
+run tomcat on port 80:
+1)
+Edit TOMCAT/conf/server.xml and change port="8080" to "80"
+2)
+sudo apt-get install authbind
+sudo touch /etc/authbind/byport/80
+sudo chmod 500 /etc/authbind/byport/80
+sudo chown ubuntu /etc/authbind/byport/80
+3) create the file TOMCAT/bin/setenv.sh with the following content:
+AUTHBIND=yes
+CATALINA_OPTS="-Djava.net.preferIPv4Stack=true"
+4) Change TOMCAT/bin/startup.sh:
+exec authbind --deep "$PRGDIR"/"$EXECUTABLE" start "$@"
+# OLD: exec "$PRGDIR"/"$EXECUTABLE" start "$@"
