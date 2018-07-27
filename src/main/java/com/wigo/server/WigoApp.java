49427f39fcd8858e785f5bc533ea50003344922c
AlexUA <alexua89@gmail.com>
2018-07-27 14:06:20 +0300

    moving to spring boot

--- src/main/java/com/wigo/server/WigoApp.java
+++ src/main/java/com/wigo/server/WigoApp.java
@@ -1,20 +1,23 @@
 package com.wigo.server;
 
 import com.wigo.server.config.WigoConfiguration;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+
+import java.util.Arrays;
 
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@ComponentScan
 @Import({WigoConfiguration.class})
-public class WigoApp extends SpringBootServletInitializer {
+public class WigoApp {
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(WigoApp.class, args);
@@ -22,11 +25,19 @@ public class WigoApp extends SpringBootServletInitializer {
         System.out.println("Visit /swagger-ui.html fro more details");
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(appClass);
-    }
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
 
-    private static Class<WigoApp> appClass = WigoApp.class;
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
+    }
 
 }
