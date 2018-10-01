bfbb95a412056d41f483844d99e41a5cbdc795e9
AlexUA <alexua89@gmail.com>
2018-10-01 20:03:15 +0300

    added FE part

---
+++ src/main/java/com/wigo/server/controllers/LandingController.java
@@ -0,0 +1,14 @@
+package com.wigo.server.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LandingController {
+
+    @GetMapping(value = "/")
+    public String index() {
+        return "index";
+    }
+
+}
