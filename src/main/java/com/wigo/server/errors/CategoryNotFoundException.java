59f78ca7a821239326ab59cbe320cccf3058b39a
AlexUA <alexua89@gmail.com>
2018-10-05 01:10:53 +0300

    Revert "add new files"
    This reverts commit 00887b1

--- src/main/java/com/wigo/server/errors/CategoryNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.wigo.server.errors;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "No such category")
-public class CategoryNotFoundException extends RuntimeException  {
-}
