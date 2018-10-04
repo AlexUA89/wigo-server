a2674b097e12eb213584026b90892936a70ac2a9
AlexUA <alexua89@gmail.com>
2018-10-04 22:38:12 +0300

    fix problems with start

--- scripts/pullAndRestart.sh
+++ scripts/pullAndRestart.sh
@@ -5,4 +5,4 @@ cd "$WIGO_DIR"
 git pull https://9565d92cc7e245e3e476c481966694e87401035f@github.com/AlexUA89/wigo-server.git
 rm -rf "$TOMCAT_DIR/target"
 mvn clean package
-java -jar ./target/server-1.0-SNAPSHOT.jar > "$TOMCAT_DIR/log.txt" 2>&1 &
+java -jar ./target/server-1.0.jar > "$TOMCAT_DIR/log.txt" 2>&1 &
