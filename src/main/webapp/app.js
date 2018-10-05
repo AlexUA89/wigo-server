c144e13e1d1b8ea6b16b2606054ad10fe7a0b399
AlexUA <alexua89@gmail.com>
2018-10-05 17:25:30 +0300

    start of application

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -2,3 +2,14 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 //const client = require('./client');
 
+import logo from './../resources/static/images/logo.svg';
+import './../resources/static/main.css';
+import Map from './components/Map.js'
+
+ReactDOM.render(<div className="App">
+   <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo"/>
+    <h1 className="App-title">Welcome to React</h1>
+    </header>
+    <Map/>
+   </div>, document.getElementById('root'));
