1283bc7d798f752b74ebcf28f3012121ba7cdfe9
deniskhomutenko <deniskhomutenko@gmail.com>
2018-10-06 22:57:45 +0300

    add maps

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -2,14 +2,10 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 //const client = require('./client');
 
-import logo from './../resources/static/images/logo.svg';
-import './../resources/static/main.css';
 import Map from './components/Map.js'
 
 ReactDOM.render(<div className="App">
-   <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo"/>
-    <h1 className="App-title">Welcome to React</h1>
+    <header className="App-header">
     </header>
     <Map/>
    </div>, document.getElementById('root'));
